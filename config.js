1dbbfc084233ddbbff1f74682a354edc308cd224
tuanday2908 <94533879+tuanday2908@users.noreply.github.com>
2021-11-17 19:10:02 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Trang ngu si",
+    introTitle: 'Trang ngu ',
+    introDesc: `cái này bấm vào, xong tuyển Ban, dm cái âm thanh cứ lúc được lúc không `,
+    btnIntro: 'Trang 50 ',
+    title: 'Phải chăng đã yêu Ban ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vào Đối Ngoại thôi  <33',
+    btnNo: 'vào cc  :3',
+    question: 'Đội có 3 Ban, tại sao lại muốn vào Đối Ngoại  <3',
+    btnReply: 'Gửi nè  <3',
+    reply: 'Vì có chị Trang  <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Vậy có điền đơn không .',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100007084541854' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
